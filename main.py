6265a63a1bebe582d055d6278d35ada67ccacbe2
Kumar Ayush <cheekujodhpur@gmail.com>
2016-05-31 18:05:47 +0530

    reorganized structure

--- main.py
+++ main.py
@@ -3,74 +3,75 @@
 import cv2
 import numpy as np
 
+# init hsv detect function
+h = 0
+cv2.namedWindow('Original')
+
 def rethsv(event,x,y,flags,param):
         global h
         if event == cv2.EVENT_LBUTTONDOWN:
             #print hsv[y,x]
             h=hsv[y,x,0]
 
-cv2.namedWindow('frame')
-cv2.setMouseCallback('frame', rethsv)  
-cv2.namedWindow('mask')  
+cv2.setMouseCallback('Original', rethsv)  
 
-
+# choose default camera
 cap = cv2.VideoCapture(0)
-h=174
 
 while(1):
     # Take each frame
- _, frame = cap.read()
- frame = cv2.flip(frame,1)
+    ret, frame = cap.read()
+    frame = cv2.flip(frame,1)
 
- cv2.imshow('frame',frame)
+    cv2.imshow('Original',frame)
 
- hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
- if h<20:
-        l=0
-        m=h+20
- elif h>245:
-        m=255
-        l=h-10
- else:
-        l=h-20
-        m=h+20
- lower = np.array([l,100,100])   
- upper = np.array([m,255,255])
- mask = cv2.inRange(hsv, lower, upper)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
- 
- res = cv2.bitwise_and(frame,frame, mask=mask)
- img = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
- img = 255-img
- params = cv2.SimpleBlobDetector_Params()
+    # define hue limits and create mask
+    if h<20:
+           l=0
+           m=h+20
+    elif h>245:
+           m=255
+           l=h-10
+    else:
+           l=h-20
+           m=h+20
+    lower = np.array([l,100,100])   
+    upper = np.array([m,255,255])
+    mask = cv2.inRange(hsv, lower, upper)
 
- params.filterByArea = True
- params.minArea = 100
-   
- detector = cv2.SimpleBlobDetector(params)
+    # generate result image
+    mask_res = cv2.bitwise_and(frame,frame, mask=mask)
 
- keypoints = detector.detect(img)
+    # convert to grayscale and invert
+    img = cv2.cvtColor(mask_res, cv2.COLOR_BGR2GRAY)
+    img = 255-img
 
- im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # Blob Detector Parameters
+    params = cv2.SimpleBlobDetector_Params()
 
- cv2.imshow("Keypoints", im_with_keypoints)
- 
+    params.filterByArea = True
+    params.minArea = 100
+      
+    detector = cv2.SimpleBlobDetector(params)
 
- cv2.imshow('frame',frame)
- #cv2.imshow('mask',mask)
- #cv2.imshow('img',img)
- if len(keypoints)>0:
-     print "x:"
-     print keypoints[0].pt[0]
-     print "y:"
-     print keypoints[0].pt[1]
- 
- k = cv2.waitKey(5) & 0xFF
- 
- if k == 27:
+    keypoints = detector.detect(img)
+
+    im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Detected and drawn to show: the blobs
+
+    cv2.imshow("Keypoints", im_with_keypoints)
+
+    # Print Keypoint position
+    if len(keypoints)>0:
+        print "x:", keypoints[0].pt[0]
+        print "y:", keypoints[0].pt[1]
+    
+    if (cv2.waitKey(5) & 0xFF) == 27:
         break
 
-
-
+# clean up
 cv2.destroyAllWindows()
 	 
