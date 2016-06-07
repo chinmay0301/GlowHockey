880638124d92aebb9f32055c147c0b97e968d097
harsh <harshmundra97@gmail.com>
2016-06-08 01:11:15 +0530

    mookund.py

--- mookund.py
+++ mookund.py
@@ -5,154 +5,9 @@ import random
 from combination import out
 import cv2
 import numpy as np
-def outx(): 
+
     
-    def rethsv(event,x,y,flags,param):
-        global h
-        if event == cv2.EVENT_LBUTTONDOWN:
-            #print hsv[y,x]
-            h=hsv[y,x,0]
-
-    cv2.namedWindow('frame')
-    cv2.setMouseCallback('frame', rethsv)  
-    cv2.namedWindow('mask')  
-
-
-    cap = cv2.VideoCapture(0)
-    h=174
-    i=0
-    while(i==0):
-        # Take each frame
-     _, frame = cap.read()
-     frame = cv2.flip(frame,1)
-
-     cv2.imshow('frame',frame)
-     
-     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-     if h<20:
-        l=0
-        m=h+20
-     elif h>245:
-        m=255
-        l=h-10
-     else:
-        l=h-20
-        m=h+20
-     lower = np.array([l,100,100])   
-     upper = np.array([m,255,255])
-     mask = cv2.inRange(hsv, lower, upper)
-
-     
-     res = cv2.bitwise_and(frame,frame, mask=mask)
-     img = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-     img = 255-img
-     params = cv2.SimpleBlobDetector_Params()
-
-     params.filterByArea = True
-     params.minArea = 50
-       
-     detector = cv2.SimpleBlobDetector(params)
-
-     keypoints = detector.detect(img)
-
-     im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-     cv2.imshow("Keypoints", im_with_keypoints)
-     
-
-     cv2.imshow('frame',frame)
-     ans = [0,0]
-     i=10
-     #cv2.imshow('mask',mask)
-     #cv2.imshow('img',img)
-     if len(keypoints)>0:
-      print "x:"
-      print keypoints[0].pt[0]
-      ans[0] = keypoints[0].pt[0]
-      print "y:"
-      print keypoints[0].pt[1]
-      ans[1] = keypoints[0].pt[1]  
-     k = cv2.waitKey(5) & 0xFF
-     
-     return keypoints[0].pt[0]
-     if k == 27:
-        break
-  
-
-def outy(): 
-    
-    def rethsv(event,x,y,flags,param):
-        global h
-        if event == cv2.EVENT_LBUTTONDOWN:
-            #print hsv[y,x]
-            h=hsv[y,x,0]
-
-    cv2.namedWindow('frame')
-    cv2.setMouseCallback('frame', rethsv)  
-    cv2.namedWindow('mask')  
-
-
-    cap = cv2.VideoCapture(0)
-    h=174
-    i=0
-    while(i==0):
-        # Take each frame
-     _, frame = cap.read()
-     frame = cv2.flip(frame,1)
-
-     cv2.imshow('frame',frame)
-     
-     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-     if h<20:
-        l=0
-        m=h+20
-     elif h>245:
-        m=255
-        l=h-10
-     else:
-        l=h-20
-        m=h+20
-     lower = np.array([l,100,100])   
-     upper = np.array([m,255,255])
-     mask = cv2.inRange(hsv, lower, upper)
-
-     
-     res = cv2.bitwise_and(frame,frame, mask=mask)
-     img = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-     img = 255-img
-     params = cv2.SimpleBlobDetector_Params()
-
-     params.filterByArea = True
-     params.minArea = 50
-       
-     detector = cv2.SimpleBlobDetector(params)
-
-     keypoints = detector.detect(img)
-
-     im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-     cv2.imshow("Keypoints", im_with_keypoints)
-     
-
-     cv2.imshow('frame',frame)
-     ans = [0,0]
-     i=10
-     #cv2.imshow('mask',mask)
-     #cv2.imshow('img',img)
-     if len(keypoints)>0:
-      print "x:"
-      print keypoints[0].pt[0]
-      ans[0] = keypoints[0].pt[0]
-      print "y:"
-      print keypoints[0].pt[1]
-      ans[1] = keypoints[0].pt[1]  
-     k = cv2.waitKey(5) & 0xFF
-     
-     
-     return keypoints[0].pt[1]
-
-     if k == 27:
-        break    
+   
               
 
 
@@ -201,6 +56,8 @@ count=0  #variables added
 t=0
 prev=pygame.time.get_ticks()/1000.0
 ai_speed=0
+a=0
+b=0
 while True:
     #lines added
     if count!=0:
@@ -226,8 +83,78 @@ while True:
     screen.blit(score1,(250.,210.))
     screen.blit(score2,(380.,210.))
     #pygame.time.wait(500)
-    bar1_y = outy
-    bar1_x = outx
+    def rethsv(event,x,y,flags,param):
+        global h
+        if event == cv2.EVENT_LBUTTONDOWN:
+            #print hsv[y,x]
+            h=hsv[y,x,0]
+
+    cv2.namedWindow('frame')
+    cv2.setMouseCallback('frame', rethsv)  
+    cv2.namedWindow('mask')  
+
+
+    cap = cv2.VideoCapture(0)
+    h=174
+    i=0
+    while(i==0):
+        # Take each frame
+     _, frame = cap.read()
+     frame = cv2.flip(frame,1)
+
+     cv2.imshow('frame',frame)
+     
+     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+     if h<20:
+        l=0
+        m=h+20
+     elif h>245:
+        m=255
+        l=h-10
+     else:
+        l=h-20
+        m=h+20
+     lower = np.array([l,100,100])   
+     upper = np.array([m,255,255])
+     mask = cv2.inRange(hsv, lower, upper)
+
+     
+     res = cv2.bitwise_and(frame,frame, mask=mask)
+     img = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+     img = 255-img
+     params = cv2.SimpleBlobDetector_Params()
+
+     params.filterByArea = True
+     params.minArea = 50
+       
+     detector = cv2.SimpleBlobDetector(params)
+
+     keypoints = detector.detect(img)
+
+     im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+     cv2.imshow("Keypoints", im_with_keypoints)
+     
+
+     cv2.imshow('frame',frame)
+     ans = [0,0]
+     i=10
+     #cv2.imshow('mask',mask)
+     #cv2.imshow('img',img)
+     if len(keypoints)>0:
+      print "x:"
+      a=keypoints[0].pt[0]
+      ans[0] = keypoints[0].pt[0]
+      print "y:"
+      b= keypoints[0].pt[1]
+      ans[1] = keypoints[0].pt[1]  
+     k = cv2.waitKey(5) & 0xFF
+     
+     if k == 27:
+        break
+  
+    bar1_y = b
+    bar1_x = a
     bar2_y += bar2_movey
     bar2_x += bar2_movex
 # movement of circle
